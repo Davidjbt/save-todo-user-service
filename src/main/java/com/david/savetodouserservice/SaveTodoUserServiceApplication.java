e0875bb1561cd2e7976f3417bd727fe7fabed154
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-06-20 14:37:49 +0100

    Initial commit

---
+++ src/main/java/com/david/savetodouserservice/SaveTodoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.david.savetodouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveTodoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveTodoUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/savetodouserservice/SaveTodoUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.savetodouserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTodoUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
