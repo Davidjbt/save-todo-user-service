7692f688da674b83448062d103af6536ad5a0e08
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-07-01 14:50:08 +0100

    Set up MySQL database connection with Flyway
    - Added DB connection config in application.yml
    - Configured MySQL environment variables in Docker compose
    - Created initial Flyway migration for User table
    - Defined ID generation strategy for User entity

--- src/main/java/com/david/savetodouserservice/User/User.java
+++ src/main/java/com/david/savetodouserservice/User/User.java
@@ -1,12 +1,15 @@
 package com.david.savetodouserservice.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public String name;
